fa08e21f04ea2917250e696f1db60f80051bdb36
Stephan Smit <stephan.smit@prorail.nl>
2023-07-09 17:30:28 +0200

    Bump version: 0.0.0 → 0.0.1

--- heliostrome/__init__.py
+++ heliostrome/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Stephan Smit"
 __email__ = "stephansmit@hotmail.com"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 
 def get_module_version():
--- setup.py
+++ setup.py
@@ -81,6 +81,6 @@ setup(
     url="https://github.com/stephansmit/heliostrome",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.0",
+    version="0.0.1",
     zip_safe=False,
 )
