6e605ba55772c1af3968ceb55112671105accbc1
Sam Parijs <Samparijs@gmail.com>
2023-09-27 15:46:56 +0200

    completed case study for north east china case. Note: location is not accurate (42.57;119.19) but this is the closest that work. In addition the mulches have been let on default but can still be explored. 2016 is a gap year, so a longer simulation was done to fix that again. This commit adds a compile for NE china, results excel file, and a plot.py. In addition a name change for the bangladesh results_plot.py file and a change in the input factors file to make it compatible with the latest case study simulation

---
+++ heliostrome/jip_project/Results_plot_NEchina.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read the Excel file
+excel_file = r'heliostrome\jip_project\results\Northeastchina_results.xlsx'
+sheet_name = "Output Results"
+df = pd.read_excel(excel_file, sheet_name=sheet_name)
+
+# Group the data by 'Case Study' and calculate the average 'Yield (tonne/ha)'
+average_yields = df.groupby('Case Study')['Yield (tonne/ha)'].mean().reset_index()
+experimental_yields = pd.read_excel(excel_file, sheet_name="Input Parameters")
+
+graphing_dataframe = pd.merge(experimental_yields[['Case Study', 'Yield (Ton/HA)']], average_yields[['Case Study', 'Yield (tonne/ha)']])
+
+# Create the bar chart
+X_axis = np.arange(len(graphing_dataframe['Case Study']))
+
+ax = graphing_dataframe.plot(kind='bar', x='Case Study', figsize=(10, 6))
+
+ax.bar(X_axis - 0.2, graphing_dataframe['Yield (Ton/HA)'], 0.4, label='Expected Output')
+ax.bar(X_axis + 0.2, graphing_dataframe['Yield (tonne/ha)'], 0.4, label='Actual Output')
+
+plt.xlabel('Case Study')
+plt.ylabel('Average Yield (tonne/ha)')
+plt.title('Average Yield by Case Study')
+plt.xticks(rotation=90)
+
+# Add legend with appropriate labels
+plt.legend(['Expected Output', 'Actual Output'])
+
+plt.tight_layout()
+plt.show()
--- heliostrome/jip_project/case_study_compiler_northeastCHINA.py
+++ heliostrome/jip_project/case_study_compiler_northeastCHINA.py
@@ -73,7 +73,7 @@ final_df = pd.DataFrame(columns=['Season', 'crop Type', 'Harvest Date (YYYY/MM/D
 final_input_df = pd.DataFrame(columns=['Case Study','Latitude','Longitude','Start Date','End Date','Soil Type', 'Crop Type','Sowing Date','Irrigation Method','SMT', 'Init WC - WC Type','init WC - Value',  'Yield (Ton/HA)', 'Water Used (mm)'])
 
 for i in range(4):
-    location = Location(latitude=extracted_rows["Latitude"][i], longitude=extracted_rows["Longitude"][i])
+    location = Location(latitude=40, longitude=114)
     start_date = extracted_rows["Start Date"][i].date()
     end_date = extracted_rows["End Date"][i].date()
     
@@ -127,6 +127,73 @@ for i in range(4):
 
     for x in range(len(df)):
         Casestudies.append(extracted_rows["Case Study"][i])
+
+    final_df = final_df.append(df, ignore_index=True)
+    input_df = pd.DataFrame(input_df)
+    final_input_df = final_input_df.append(input_df, ignore_index=True)
+
+
+    #time elapsed
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Iteration {i+1}: Elapsed time = {elapsed_time} seconds")
+    start_time = end_time
+
+for i in range(2):
+    location = Location(latitude=40, longitude=114)
+    start_date = extracted_rows["Start Date"][i+4].date()
+    end_date = extracted_rows["End Date"][i+4].date()
+    
+    climate_data = ClimateData(
+        location=location,
+        start_date=start_date,
+        end_date=end_date,
+    )
+
+    climate_data.plot_data(y_axis='temp_air_max_c')
+
+    soil = Soil(extracted_rows["Soil Type"][i+4])
+    crop = get_crop_data(extracted_rows["Crop Type"][i+4])
+    sowing_date = extracted_rows["Sowing Date"][i+4].strftime("%m/%d")
+    crop = Crop(crop.Name, planting_date=sowing_date)
+    irr_mngt = IrrigationManagement(irrigation_method=0)
+    InitWC = InitialWaterContent(wc_type = 'Pct', value=[extracted_rows["Initial Water Content"][i+4]])
+    
+    
+    
+    input_df = {'Case Study': [extracted_rows["Case Study"][i+4]],
+                'Latitude' : [location.latitude],
+                'Longitude' :[location.longitude],
+                'Start Date' : [start_date],
+                'End Date' : [end_date],
+                'Soil Type' : [soil.Name], 
+                'Crop Type' : [crop.Name],
+                'Sowing Date' :[sowing_date],
+                'Irrigation Method' : [irr_mngt.irrigation_method],
+                'SMT' : [irr_mngt.SMT], 
+                'Init WC - WC Type' :[InitWC.wc_type],
+                'Init WC - Value': [InitWC.value],
+                'Yield (Ton/HA)': [extracted_rows['Yield'][i+4]],
+                'Water Used (mm)': [extracted_rows['Water used'][i+4]]}
+
+
+    model = AquaCropModel(
+        sim_start_time=start_date.strftime("%Y/%m/%d"),
+        sim_end_time=end_date.strftime("%Y/%m/%d"),
+        weather_df=climate_data.aquacrop_input,
+        soil=soil,
+        crop=crop,
+        initial_water_content=InitWC,
+        irrigation_management=irr_mngt, 
+        field_management=FieldMngt(mulches= bool(extracted_rows["Mulches"][i+4] == "True")),
+    )
+    
+    model.run_model(till_termination=True)
+
+    df = model.get_simulation_results()
+
+    for x in range(len(df)):
+        Casestudies.append(extracted_rows["Case Study"][i+4])
 
     final_df = final_df.append(df, ignore_index=True)
     input_df = pd.DataFrame(input_df)
