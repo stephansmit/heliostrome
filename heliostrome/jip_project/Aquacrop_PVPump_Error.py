5ed66b080ecbc60ff44c348276d633d50a4c227b
gpans <125491092+gpans@users.noreply.github.com>
2023-10-29 13:42:23 +0100

    tsting the mean percentage error function

--- heliostrome/jip_project/Aquacrop_PVPump_Error.py
+++ heliostrome/jip_project/Aquacrop_PVPump_Error.py
@@ -7,4 +7,10 @@ resample_and_save_weekly(r'heliostrome\jip_project\results\PVPUmp_Data.xlsx',r'h
 waterfluxexcelpath = r'heliostrome\jip_project\results\weekly_WaterFlux_Bangladesh.xlsx'
 pump_df_path = r'heliostrome\jip_project\results\weekly_Pump_Data.xlsx'
 
-pump_compatibility(waterfluxexcelpath,pump_df_path)
+merged_df = pump_compatibility(waterfluxexcelpath,pump_df_path)
+
+waterflux_df = pd.read_excel(waterfluxexcelpath)
+pump_df = pd.read_excel(pump_df_path)
+
+mpe_indiv, mpe_df_avg = mean_percentage_error(merged_df,'IrrDay',merged_df,'Pump 1')
+
