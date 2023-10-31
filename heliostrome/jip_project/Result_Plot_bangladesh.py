911d57e4af1a820ebf69df02658c6584883a2f0b
Sam Parijs <Samparijs@gmail.com>
2023-10-31 17:43:59 +0100

    morroco casestudy update and fix results bangalade

--- heliostrome/jip_project/Result_Plot_bangladesh.py
+++ heliostrome/jip_project/Result_Plot_bangladesh.py
@@ -52,29 +52,41 @@ std_yields = df.groupby('Case Study')['Yield (tonne/ha)'].std().reset_index()
 experimental_yields = pd.read_excel(excel_file, sheet_name="Input Parameters")
 
 # Calculate the Mean Bias Error (MBE) in percentage
-merged_df = pd.merge(experimental_yields[['Case Study', 'Yield (Ton/HA)']], average_yields[['Case Study', 'Yield (tonne/ha)']], on='Case Study')
+merged_df = pd.merge(experimental_yields[['Case Study', 'Yield (Ton/HA)']], average_yields, on='Case Study')
 merged_df['MBE'] = ((merged_df['Yield (tonne/ha)'] - merged_df['Yield (Ton/HA)']) / merged_df['Yield (Ton/HA)']) * 100
 
 # Create a single DataFrame with Case Study as the index
-graphing_dataframe = pd.merge(experimental_yields[['Case Study', 'Yield (Ton/HA)']], average_yields[['Case Study', 'Yield (tonne/ha)']])
+graphing_dataframe = pd.merge(experimental_yields[['Case Study', 'Yield (Ton/HA)'], average_yields, on='Case Study')
 graphing_dataframe.set_index('Case Study', inplace=True)
 
 # Create a bar chart with crop yield, standard deviation, and MBE in the same graph
-ax = graphing_dataframe.plot(kind='bar', figsize=(10, 6))
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Plot the bar chart for crop yield
+bar_width = 0.35
+bar_positions = range(len(graphing_dataframe))
+ax.bar(bar_positions, graphing_dataframe['Yield (tonne/ha)'], bar_width, label='Yield (tonne/ha)')
 
 # Plot standard deviation as error bars
-ax.errorbar(graphing_dataframe.index, graphing_dataframe['Yield (tonne/ha)'], yerr=std_yields['Yield (tonne/ha)'], fmt='o', label='Yield (tonne/ha)')
+ax.errorbar([pos + bar_width/2 for pos in bar_positions], graphing_dataframe['Yield (tonne/ha)'], yerr=std_yields['Yield (tonne/ha)'], fmt='o', label='Standard Deviation')
 
-# Plot MBE as a line plot
-ax.plot(graphing_dataframe.index, merged_df['MBE'], marker='o', color='r', label='MBE (%)')
+# Create a second y-axis for MBE
+ax2 = ax.twinx()
+ax2.plot(bar_positions, merged_df['MBE'], marker='o', color='r', label='MBE (%)')
 
-plt.xlabel('Case Study')
-plt.ylabel('Yield (tonne/ha)')
-plt.title('Crop Yield, Standard Deviation, and Mean Bias Error(MBE)')
-plt.xticks(rotation=90)
+# Set the labels and title
+ax.set_xlabel('Case Study')
+ax.set_ylabel('Yield (tonne/ha)')
+ax2.set_ylabel('MBE (%)')
+plt.title('Crop Yield, Standard Deviation, and Mean Bias Error (MBE)')
+
+# Set the x-tick labels and rotation
+ax.set_xticks([pos + bar_width/2 for pos in bar_positions])
+ax.set_xticklabels(graphing_dataframe.index, rotation=90)
 
 # Add legend with appropriate labels
-plt.legend(['Yield (tonne/ha)', 'Standard Deviation', 'MBE (%)'])
+ax.legend(loc='upper left')
+ax2.legend(loc='upper right')
 
 plt.tight_layout()
 plt.show()
