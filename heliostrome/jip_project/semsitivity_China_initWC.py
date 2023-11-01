004436a7d1ec870f7d2a0755304f0e382c6e2aac
Mengying <chenmy@whu.edu.cn>
2023-11-01 15:52:01 +0100

    sensitivity China soil

--- heliostrome/jip_project/semsitivity_China_initWC.py
+++ heliostrome/jip_project/semsitivity_China_initWC.py
@@ -2,8 +2,8 @@ from datetime import datetime
 from heliostrome.models.location import Location
 from heliostrome.models.climate import ClimateData
 from aquacrop.core import IrrigationManagement
-from aquacrop.entities.irrigationManagement import IrrMngtStruct
-from aquacrop import Crop, InitialWaterContent, Soil, AquaCropModel, FieldMngt
+from aquacrop.entities.fieldManagement import FieldMngt
+from aquacrop import Crop, InitialWaterContent, Soil, AquaCropModel
 from heliostrome.data_collection.crops import get_crop_data
 from heliostrome.models.aquacrop_results import (
     SimulationResult,
@@ -17,12 +17,16 @@ from datetime import datetime
 from datetime import date
 import pandas as pd 
 import altair as alt
-from openpyxl import load_workbook
+from openpyxl import load_workbook #added!
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-from modules.irrigation_schedule_morrocco_wheat import IRRschedule
+
 from modules.Load_excel import factors_to_run
+from modules.waterflux_extraction import *
+from modules.Pump_module import *
+from modules.precip_extract import *
+
 
 # Start the timer 
 start_time = time.time()
@@ -30,35 +34,20 @@ start_time = time.time()
 sheet_name = "Northeast China Case Study"  # Replace with the name of the sheet containing the data
 extracted_rows = factors_to_run(sheet_name)
 
-# Define the list of soil types
-soil_types = [
-    "Clay",
-    "ClayLoam",
-    "Loam",
-    "LoamySand",
-    "Sand",
-    "SandyClay",
-    "SandyClayLoam",
-    "SandyLoam",
-    "Silt",
-    "SiltClayLoam",
-    "SiltLoam",
-    "SiltClay",
-    "Paddy",
-    "ac_TunisLocal",
+
+WC_values = [
+    "FC",
+    "SAT",
+    "WP"
 ]
 
-# Create an Excel writer to save the results
-writer = pd.ExcelWriter(r'heliostrome\jip_project\results\sensitivity_China_soil.xlsx', engine='openpyxl')
-
+writer = pd.ExcelWriter(r'heliostrome\jip_project\results\sensitivity_China_initWC.xlsx', engine='openpyxl')
 
 alt.data_transformers.enable("default", max_rows=None)
 
-
-
-for soil_type in soil_types:
-    final_input_df = pd.DataFrame(columns=['Case Study','Latitude','Longitude','Start Date','End Date','Soil Type', 'Crop Type','Sowing Date','Irrigation Method','SMT', 'Init WC - WC Type','init WC - Value',  'Yield (Ton/HA)', 'Water Used (mm)'])
+for WC_value in WC_values:
     final_df = pd.DataFrame(columns=['Season', 'crop Type', 'Harvest Date (YYYY/MM/DD)', 'Harvest Date (Step)', 'Yield (tonne/ha)', 'Seasonal irrigation (mm)'])
+    final_input_df = pd.DataFrame(columns=['Case Study','Latitude','Longitude','Start Date','End Date','Soil Type', 'Crop Type','Sowing Date','Irrigation Method','SMT', 'Init WC - WC Type','init WC - Value',  'Yield (Ton/HA)', 'Water Used (mm)'])
     Casestudies = []
 
 
@@ -75,7 +64,7 @@ for soil_type in soil_types:
 
         climate_data.plot_data(y_axis='temp_air_max_c')
         print(extracted_rows["Sowing Date"][i])
-        soil = Soil(soil_type)
+        soil = Soil(extracted_rows["Soil Type"][i])
         crop = get_crop_data(extracted_rows["Crop Type"][i])
         sowing_date = extracted_rows["Sowing Date"][i].strftime("%m/%d")
         crop = Crop(crop.Name, planting_date=sowing_date)
@@ -125,6 +114,7 @@ for soil_type in soil_types:
         #waterflux related lines
         water_flux = model._outputs.water_flux
         sheet_name = f"{extracted_rows['Case Study'][i]}"
+        water_flux.to_excel(writer1, index=False, sheet_name=sheet_name)
 
 
         #time elapsed
@@ -146,7 +136,7 @@ for soil_type in soil_types:
 
         climate_data.plot_data(y_axis='temp_air_max_c')
 
-        soil = Soil(soil_type)
+        soil = Soil(extracted_rows["Soil Type"][i+4])
         crop = get_crop_data(extracted_rows["Crop Type"][i+4])
         sowing_date = extracted_rows["Sowing Date"][i+4].strftime("%m/%d")
         crop = Crop(crop.Name, planting_date=sowing_date)
@@ -197,23 +187,10 @@ for soil_type in soil_types:
         #waterflux related lines
         water_flux = model._outputs.water_flux
         sheet_name = f"{extracted_rows['Case Study'][i+4]}"
-        # water_flux.to_excel(writer1, index=False, sheet_name=sheet_name)
+        water_flux.to_excel(writer1, index=False, sheet_name=sheet_name)
 
         #time elapsed
         end_time = time.time()
         elapsed_time = end_time - start_time
         print(f"Iteration {i+1}: Elapsed time = {elapsed_time} seconds")
         start_time = end_time
-    
-    # Insert the 'Case Study' column to final_df
-    final_df.insert(0, 'Case Study', Casestudies)
-    
-    # Save the results to a separate sheet with the soil type as the sheet name
-    final_input_df.to_excel(writer, index=False, sheet_name=soil_type + "_Input_Parameters")
-    final_df.to_excel(writer, index=False, sheet_name=soil_type + "_Output_Results")
-    
-    Casestudies.clear()  # Clear the list for the next soil type
-
-# Close the Excel writer
-writer.save()
-writer.close()
