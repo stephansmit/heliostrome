03744cec1c68c4cfb396389961a2908869a2aa0d
Mengying <chenmy@whu.edu.cn>
2023-11-06 22:15:50 +0100

    new graph for sensitivity

---
+++ heliostrome/jip_project/sensitivity_Mean_China_soil.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+
+input_file_path = r'heliostrome\jip_project\results\sensitivity_China_soil.xlsx'
+output_file_path = r'heliostrome\jip_project\results\sensitivity_Mean_China_soil.xlsx'
+
+soil_types = [
+    "Clay",
+    "ClayLoam",
+    "Loam",
+    "LoamySand",
+    "Sand",
+    "SandyClay",
+    "SandyClayLoam",
+    "SandyLoam",
+    "Silt",
+    "SiltClayLoam",
+    "SiltLoam",
+    "SiltClay",
+    "Paddy",
+    "ac_TunisLocal",
+]
+
+# read all input and output tables
+input_sheet_names = [f"{soil_type}_Input_Parameters" for soil_type in soil_types]
+output_sheet_names = [f"{soil_type}_Output_Results" for soil_type in soil_types]
+
+# create an empty excel
+writer = pd.ExcelWriter(output_file_path, engine='openpyxl')
+
+# crate an empty DataFrame to store the output of RMSE
+rmse_df = pd.DataFrame(columns=['soil type', 'Yield','Yield Actually','Water Used','Water Used Actually'])
+
+# 
+for input_sheet_name, output_sheet_name in zip(input_sheet_names, output_sheet_names):
+    
+    input_df = pd.read_excel(input_file_path, sheet_name=input_sheet_name)
+    output_df = pd.read_excel(input_file_path, sheet_name=output_sheet_name)
+
+    # merge the two table
+    matched_data = pd.merge(output_df, input_df, on='Case Study', how='inner')
+    # Yield RMSE
+    yield_mean = np.mean(matched_data['Yield (tonne/ha)'])
+    yield_a_mean = np.mean(matched_data['Yield (Ton/HA)'])
+    
+    # Water Used Mean
+    water_used_mean = np.mean(matched_data['Seasonal irrigation (mm)'])
+    
+    # Water Use Actually
+    water_mean = 30.01
+
+
+    soil_type = input_df['Soil Type'].iloc[0]
+
+    # ouput
+    df_to_append = pd.DataFrame({'soil type': soil_type, 'Yield': yield_mean,'Yield Actually': yield_a_mean, 'Water Used': water_used_mean,'Water Used Actually':water_mean}, index=[0])
+    rmse_df = pd.concat([rmse_df, df_to_append], ignore_index=True)
+
+# 将 RMSE 结果保存到新 Excel 文件
+rmse_df.to_excel(writer, index=False, sheet_name='Mean Results')
+
+# 关闭 Excel writer
+writer.save()
--- heliostrome/jip_project/sensitivity_plot_soil.py
+++ heliostrome/jip_project/sensitivity_plot_soil.py
@@ -1,95 +1,54 @@
+import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Existing data for soil type
-locations = ['Bangladesh'] * 14
-soil_type = ['Clay', 'ClayLoam', 'Loam', 'LoamySand', 'Sand', 'SandyClay', 'SandyClayLoam', 'SandyLoam', 'Silt', 'SiltClayLoam', 'SiltLoam', 'SiltClay', 'Paddy', 'ac_TunisLocal']
-yield_rmse = [38.75945715, 31.58564166, 31.58502913, 31.58502365, 31.58502365, 31.59291301, 31.58503875, 31.58502365, 31.5853929, 31.58525588, 31.58504707, 34.4411245, 35.70949357, 31.71743356]
-yield_rmse_percentage = [101.4206824, 82.64917953, 82.64757674, 82.64756239, 82.64756239, 82.66820625, 82.64760191, 82.64756239, 82.64852859, 82.64817006, 82.64762367, 90.12103386, 93.4399363, 82.99403536]
-water_used_rmse = [170.0926316, 323.751363, 332.3115709, 341.3426932, 337.650014, 322.1579658, 326.4977306, 345.0297398, 332.9589972, 324.344311, 334.928779, 252.2981169, 230.7853976, 318.6975205]
-water_used_rmse_percentage = [69.11891392, 131.5597411, 135.0382709, 138.7081615, 137.2076029, 130.9122476, 132.6757562, 140.2064314, 135.3013593, 131.8006918, 136.1018006, 102.5239698, 93.78205201, 129.5060596]
+# 读取数据
+path_china = r'heliostrome\jip_project\results\sensitivity_Mean_China_soil.xlsx'
+path_bangladesh = r'heliostrome\jip_project\results\sensitivity_Mean_Bangladesh_soil.xlsx'
+path_morocco = r'heliostrome\jip_project\results\sensitivity_Mean_Morocco_soil.xlsx'
 
-# Colors
-colors = {
-    'Clay': 'red',
-    'ClayLoam': 'blue',
-    'Loam': 'green',
-    'LoamySand': 'purple',
-    'Sand': 'orange',
-    'SandyClay': 'pink',
-    'SandyClayLoam': 'brown',
-    'SandyLoam': 'cyan',
-    'Silt': 'magenta',
-    'SiltClayLoam': 'lime',
-    'SiltLoam': 'gray',
-    'SiltClay': 'olive',
-    'Paddy': 'navy',
-    'ac_TunisLocal': 'teal',
-}
+# 用Pandas读取数据
+data_china = pd.read_excel(path_china)
+data_bangladesh = pd.read_excel(path_bangladesh)
+data_morocco = pd.read_excel(path_morocco)
 
-# Create subplots
-fig, (ax1, ax3) = plt.subplots(1, 2, figsize=(14, 6))
+# 定义一个绘图函数
+def plot_data(data, country_name):
+    fig, ax1 = plt.subplots(figsize=(12, 6))
 
-# Bar positions
-bar_positions = np.arange(len(locations))
+    # 设置横轴标签
+    soil_types = data['soil type']
+    index = range(len(soil_types))
+    
+    # 绘制柱状图
+    ax1.bar(index, data['Yield'], width=0.4, label='Yield', color='darkorange', align='center')
+    ax1.set_xlabel('Soil Type')
+    ax1.set_ylabel('Yield', color='darkorange')
+    ax1.tick_params('y', colors='darkorange')
+    ax1.set_xticks(index)
+    ax1.set_xticklabels(soil_types, rotation=90)
 
-# Plot Yield RMSE
-for soil in set(soil_type):
-    soil_data_indices = [i for i, s in enumerate(soil_type) if s == soil]
-    ax1.bar([bar_positions[i] for i in soil_data_indices], [yield_rmse[i] for i in soil_data_indices], width=0.35, align='center', label=f'{soil}', color=colors[soil], alpha=0.9)
+    # 设置第二个y轴
+    ax2 = ax1.twinx()
+    ax2.bar([i+0.2 for i in index], data['Water Used'], width=0.4, label='Water Used', color='g', align='center',alpha=0.8)
+    ax2.set_ylabel('Water Used', color='g')
+    ax2.tick_params('y', colors='g')
 
-# Labels and titles
-ax1.set_title('Yield RMSE and Percentage RMSE by Soil Type')
-ax1.set_ylabel('Yield RMSE')
-ax1.set_xticks(bar_positions)
-ax1.set_xticklabels(locations, rotation=45, ha='right')
-ax1.set_ylim(0, max(yield_rmse) * 1.2)
+    # 绘制折线图（均值）
+    ax1.plot(index, [data['Yield Actually'].mean()]*len(index), color='brown', linestyle='--', label='Yield Actually')
+    ax2.plot(index, [data['Water Used Actually'].mean()]*len(index), color='y', linestyle='--', label='Water Used Actually')
 
-# Adding text labels
-for i, pos in enumerate(bar_positions):
-    ax1.text(pos, yield_rmse[i] + 1, f'{yield_rmse[i]:.2f}', ha='center', va='bottom', fontsize=10, color='black')
+    # 标题和图例
+    plt.title(f'Soil Type Sensitivity Analysis for {country_name}')
 
-# Add second y-axis
-ax2 = ax1.twinx()
-ax2.plot(bar_positions, yield_rmse_percentage, marker='o', color=(60/255, 64/255, 91/255), label='Percentage RMSE')
-ax2.set_ylabel('Yield Percentage RMSE')
-ax2.set_ylim(0, max(yield_rmse_percentage) * 1.1)
+    # 设置图例位置
+    ax1.legend(loc='upper left', bbox_to_anchor=(1.1, 1))
+    ax2.legend(loc='upper left', bbox_to_anchor=(1.1, 0.85))
 
-# Adding labels to line plot data points
-for i, (x, y) in enumerate(zip(bar_positions, yield_rmse_percentage)):
-    ax2.text(x, y + 1, f'{y:.2f}', ha='center', va='bottom', fontsize=10, color='black')
+    fig.tight_layout()
+    # 显示图表
+    plt.show()
 
-# Plot for Water Used RMSE
-for soil in set(soil_type):
-    soil_data_indices = [i for i, s in enumerate(soil_type) if s == soil]
-    ax3.bar([bar_positions[i] for i in soil_data_indices], [water_used_rmse[i] for i in soil_data_indices], width=0.35, align='center', label=f'{soil}', color=colors[soil], alpha=0.9)
-
-# Labels and titles
-ax3.set_title('Water Used RMSE and Percentage RMSE by Soil Type')
-ax3.set_ylabel('Water Used RMSE')
-ax3.set_xticks(bar_positions)
-ax3.set_xticklabels(locations, rotation=45, ha='right')
-ax3.set_ylim(0, max(water_used_rmse) * 1.2)
-
-# Adding text labels
-for i, pos in enumerate(bar_positions):
-    ax3.text(pos, water_used_rmse[i] + 1, f'{water_used_rmse[i]:.2f}', ha='center', va='bottom', fontsize=10, color='black')
-
-# Add second y-axis
-ax4 = ax3.twinx()
-ax4.plot(bar_positions, water_used_rmse_percentage, marker='o', color=(60/255, 64/255, 91/255), label='Water Used Percentage RMSE')
-ax4.set_ylabel('Water Used Percentage RMSE')
-ax4.set_ylim(0, max(water_used_rmse_percentage) * 1.1)
-
-# Adding labels to line plot data points
-for i, (x, y) in enumerate(zip(bar_positions, water_used_rmse_percentage)):
-    ax4.text(x, y + 1, f'{y:.2f}', ha='center', va='bottom', fontsize=10, color='black')
-
-# Add legends
-ax1.legend(title='Soil Type', loc='upper right',)
-ax2.legend(loc='upper left')
-ax3.legend(title='Soil Type', loc='upper right')
-ax4.legend(loc='upper left')
-
-plt.tight_layout()
-plt.show()
+# 分别为三个国家绘图
+plot_data(data_china, 'China')
+plot_data(data_bangladesh, 'Bangladesh')
+plot_data(data_morocco, 'Morocco')
